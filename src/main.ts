16d5cc2b4467c8557f1258cc3c785031584db005
Victor Rodrigues <victor.rodrigues@bitfy.app>
2023-05-06 16:49:09 -0300

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(
     new AuthenticationInterceptor(app.get(JwtService)),
