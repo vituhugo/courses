115684a8d71e8e2419da4216fb80ea308fc18eab
Victor Rodrigues <victor.rodrigues@bitfy.app>
2023-05-07 17:59:15 -0300

    fix: db username

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         type: 'postgres',
         host: process.env.DB_HOST ?? 'localhost',
         port: 5432,
-        username: process.env.DB_NAME ?? 'postgres',
+        username: process.env.DB_USER ?? 'postgres',
         password: process.env.DB_PASS ?? 'postgres',
         database: process.env.DB_NAME ?? 'postgres',
         entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
